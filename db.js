c8240c89a109c25d9b00c7e1cddb19f76ca70aff
Quyet <c3lttrong.0a9.ntmquyet@gmail.com>
2025-10-18 23:00:36 +0700

    thêm thư viện format code prettier + sửa api loign

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
-const mysql = require('mysql2');
+const mysql = require("mysql2");
 
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',  
-  password: 'manhquyet1362006',  
-  database: 'quanlicuahang'
+    host: "localhost",
+    user: "root",
+    password: "manhquyet1362006",
+    database: "quanlicuahang",
 });
 
 const db = pool.promise();
